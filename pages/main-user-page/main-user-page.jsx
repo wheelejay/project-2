018cfb72129f5647bf6162f7e48c1a0ed2a46d05
wheelejay <wheeler7591@gmail.com>
2023-12-19 21:28:04 -0700

    Skeleton Main user page

--- pages/main-user-page/main-user-page.jsx
+++ pages/main-user-page/main-user-page.jsx
@@ -1,1 +1,29 @@
 import { useState } from "react";
+
+export default function mainUserPage() {
+
+    return (
+        <div>
+            {/* TO DO create custom greeting with users first name*/}
+            <h1>Hello, First Name</h1>
+            {/* TO DO API for inspirational Fitness Quote*/}
+            <h2>Inspirational Fitness Quote</h2>
+            <button>Add New Submission</button><br /><br />
+            <label for="lentry">Last Entry</label><br />
+            {/* TO DO input data using the last data point recorded*/}
+            <input type="text" id="lentry" value="Last data point entered"></input><br /><br />
+            <label for="pchart">Personal Weight Loss Progress</label><br />
+            {/* TO DO create graph that charts the progress of weight loss over time*/}
+            <div></div><br /><br />
+            <label for="gweight">Goal Weight</label><br />
+             {/* TO DO insert goal weight */}
+            <input type="text" id="gweight" value="Goal Weight Value"></input><br /><br />
+             {/* TO DO button will take user to leaderboard page*/}
+            <button>Leader Board</button>{"          "}
+            <label for="timeleft">Time left in Competition</label>
+             {/* TO DO count down timer for the time left in the competition*/}
+            <input type="text" id="timeleft" value="DAYS:HOURS:MIN:SEC"></input><br /><br />
+
+        </div>
+    )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 import nameAndDescription from '../pages/login-page/app-name-description'
 import login from '../pages/login-page/login'
 import createUser from '../pages/create-user-page/create-user-page'
+import mainUserPage from '../pages/main-user-page/main-user-page'
+
 
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (createUser()
+  return (mainUserPage()
 
   )
 }
