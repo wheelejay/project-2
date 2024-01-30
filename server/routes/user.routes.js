93d65047ef39236f9d010ce97beaaa8a5917373b
Stephen Thomas <stephenthomas@Stephens-MacBook-Pro.local>
2024-01-29 20:06:00 -0700

    add user weights

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -2,6 +2,7 @@
 import { Router } from "express";
 import bcrypt from "bcryptjs";
 import { User } from "../models/userModel.js";
+import { UserWeight } from "../models/userWeightModel.js";
 
 const userRouter =  Router();
 userRouter.post('/api/users', async(req, res) =>{
@@ -13,5 +14,8 @@ userRouter.post('/api/users', async(req, res) =>{
 })
 userRouter.get('/api/users/:userId', async(req, res) => {
     User.findOne({ where: { id: req.params.userId }}).then(user => res.send({ user }));
+})
+userRouter.get('api/users/:userId/weights', async(req, res) => {
+    UserWeight.findAll({ where: { userId: req.params.userId }}).then(weights => res.send({ weights }));
 })
 export default userRouter;
